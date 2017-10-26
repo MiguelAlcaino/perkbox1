8451df666ff160c48a105d7fe3fa65ed6cdf11c0
Miguel Alcaino <miguel.alcaino@ogb.cl>
2017-10-26 02:21:57 +0100

    Catching possible exceptions when doing the GET request

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Coupon;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -26,13 +25,18 @@ class DefaultController extends Controller
             'brand' => $request->query->get('brand'),
             'value' => $request->query->get('value')
         ];
-        $couponsResponse = \Unirest\Request::get(
-            $this->getParameter('coupons_api_host').':'.$this->getParameter('coupons_api_port').'/coupons',
-            $headers,
-            $query
-        );
-
-        $response->setData($couponsResponse->body);
+        try{
+            $couponsResponse = \Unirest\Request::get(
+                $this->getParameter('coupons_api_host').':'.$this->getParameter('coupons_api_port').'/coupons',
+                $headers,
+                $query
+            );
+            $response->setData($couponsResponse->body);
+        }catch (\Exception $exception){
+            $response->setData([
+                'message' => 'Node.js REST API is not running. Follow the instructions here https://github.com/MiguelAlcaino/perkbox2 to run it and try again.'
+            ])->setStatusCode(Response::HTTP_BAD_GATEWAY);
+        }
 
         return $response;
     }
